ec7eb9ebbff909ef317fd7cf4d91784cd7f9c579
Guillermo Plaza <alu0101495354@ull.edu.es>
2024-10-24 17:44:55 +0000

    añade función para las condiciones de bernstein

--- lib/conjuntos.rb
+++ lib/conjuntos.rb
@@ -5,3 +5,16 @@ end
 def conjunto_escritura(instrucciones)
   instrucciones.flat_map { |instruccion| instruccion[:escritura] }.uniq
 end
+
+def verificar_condiciones_bernstein(instrucciones_1, instrucciones_2)
+  lectura_1 = conjunto_lectura(instrucciones_1)
+  escritura_1 = conjunto_escritura(instrucciones_1)
+  lectura_2 = conjunto_lectura(instrucciones_2)
+  escritura_2 = conjunto_escritura(instrucciones_2)
+
+  no_interferencia_1 = (escritura_1 & lectura_2).empty?
+  no_interferencia_2 = (escritura_2 & lectura_1).empty?
+  no_interferencia_escritura = (escritura_1 & escritura_2).empty?
+
+  no_interferencia_1 && no_interferencia_2 && no_interferencia_escritura
+end
