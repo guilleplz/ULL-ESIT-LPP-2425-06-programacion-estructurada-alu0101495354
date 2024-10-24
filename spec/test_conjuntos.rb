bf26b27f9adf4b9670d843326def873ab0feb30d
Guillermo Plaza <alu0101495354@ull.edu.es>
2024-10-24 17:27:46 +0000

    añade primer test y variables para probar el conjunto de escritura (falla)

---
+++ spec/test_conjuntos.rb
@@ -0,0 +1,37 @@
+require 'conjuntos'
+
+# frozen_string_literal: true
+
+describe "Condiciones de Bernstein" do
+
+  # Declaración de variables para los tests
+
+  let(:instrucciones_1) do
+    [
+      { lectura: ['a'], escritura: ['b'] },
+      { lectura: ['b'], escritura: ['c'] }
+    ]
+  end
+
+  let(:instrucciones_2) do
+    [
+      { lectura: ['d'], escritura: ['e'] },
+      { lectura: ['e'], escritura: ['f'] }
+    ]
+  end
+
+  let(:instrucciones_conflictivas) do
+    [
+      { lectura: ['b'], escritura: ['a'] },
+      { lectura: ['c'], escritura: ['d'] }
+    ]
+  end
+
+
+  describe '#conjunto_lectura' do
+    it 'devuelve el conjunto correcto de lectura' do
+      expect(conjunto_lectura(instrucciones_1)).to eq(['a', 'b'])
+    end
+  end
+
+end
