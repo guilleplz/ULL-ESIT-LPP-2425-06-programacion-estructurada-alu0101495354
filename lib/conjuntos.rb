97f1025dccfb4289c98d3997e930d7e599610d23
Guillermo Plaza <alu0101495354@ull.edu.es>
2024-10-24 17:40:34 +0000

    añade función conjunto_escritura

--- lib/conjuntos.rb
+++ lib/conjuntos.rb
@@ -1,3 +1,7 @@
 def conjunto_lectura(instrucciones)
   instrucciones.flat_map { |instruccion| instruccion[:lectura] }.uniq
 end
+
+def conjunto_escritura(instrucciones)
+  instrucciones.flat_map { |instruccion| instruccion[:escritura] }.uniq
+end
